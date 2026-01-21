6f8391769a53cd369decb4448b7a0bb87135ac42
PurpleDeving <111588944+PurpleDeving@users.noreply.github.com>
2026-01-21 21:45:50 +0100

    Fix the Attributes later

--- src/main/java/net/purple/permfood/moddata/attributes/PlayerAttributeEvents.java
+++ src/main/java/net/purple/permfood/moddata/attributes/PlayerAttributeEvents.java
@@ -1,10 +1,8 @@
 package net.purple.permfood.moddata.attributes;
 
-import net.minecraft.world.entity.EntityType;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.event.entity.EntityAttributeModificationEvent;
-import net.purple.permfood.config.ConfigAttributes;
 
 import static net.purple.permfood.PermanentFood.MODID;
 
@@ -15,9 +13,9 @@ public class PlayerAttributeEvents {
     public static void modifyDefaultAttributes(EntityAttributeModificationEvent event) {
 
 
-        if (ConfigAttributes.ENABLE_MAX_HUNGER_CHANGES.get()) {
+/*        if (ConfigAttributes.ENABLE_MAX_HUNGER_CHANGES.get()) {
             event.add(EntityType.PLAYER, ModAttributes.MAX_HUNGER, ConfigAttributes.NEW_BASE_MAX_HUNGER.get());
-        }
+        }*/
 /*
         if (ConfigAttributes.ENABLE_MAX_SATURATION_CHANGES.get()) {
             event.add(EntityType.PLAYER, ModAttributes.MAX_HUNGER, ConfigAttributes.NEW_BASE_MAX_SATURATION.get());
