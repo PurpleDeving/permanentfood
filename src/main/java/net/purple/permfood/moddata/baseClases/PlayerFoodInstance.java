af2d712a25184bf2aa15b51fc330bc0bf440d44d
PurpleDeving <111588944+PurpleDeving@users.noreply.github.com>
2026-01-21 19:38:56 +0100

    -

--- src/main/java/net/purple/permfood/moddata/baseClases/PlayerFoodInstance.java
+++ src/main/java/net/purple/permfood/moddata/baseClases/PlayerFoodInstance.java
@@ -10,7 +10,7 @@ import java.util.List;
 public abstract class PlayerFoodInstance {
 
     private int foodCount = 0;
-    private Player player;
+    private final Player player;
 
     private final List<MilestoneBased> MilestoneBasedList = new ArrayList<>();
 
