75d4e93dc2ee902abcf7f2f69341144a4c83a2fa
PurpleDeving <111588944+PurpleDeving@users.noreply.github.com>
2026-02-04 16:36:51 +0100

    Client Bound Celebration Part 1

--- src/main/java/net/purple/solextended/foodlist/FoodListEatEvent.java
+++ src/main/java/net/purple/solextended/foodlist/FoodListEatEvent.java
@@ -1,7 +1,7 @@
 package net.purple.solextended.foodlist;
 
 
-import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.player.Player;
 import net.neoforged.bus.api.EventPriority;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
@@ -9,6 +9,9 @@ import net.neoforged.fml.util.thread.EffectiveSide;
 import net.neoforged.neoforge.event.entity.living.LivingEntityUseItemEvent;
 import net.purple.solextended.api.milestonebased.MilestoneManagerRegistry;
 import net.purple.solextended.config.Configs;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static net.purple.solextended.Constants.ENABLE_EXTENSIVE_LOGGING;
 import static net.purple.solextended.SolExtended.*;
@@ -30,16 +33,15 @@ public class FoodListEatEvent {
             LOGGER.info("FoodListEvents.onFoodEaten triggered");
         }
 
-
-        if (EffectiveSide.get().isClient()) {
-            return;
-        }
-        ServerPlayer player = (ServerPlayer) event.getEntity();
+        Player player = (Player) event.getEntity();
 
         if (Configs.solExtendedConfig.limitProgressToSurvival && player.isCreative()) return;
 
+
         var usedStack = event.getItem();
         if (usedStack.getFoodProperties(player) == null) return;
+
+        player.syncData(FOOD_LIST_ATTACHMENT);
 
         PlayerFoodList playerFoodList = player.getData(FOOD_LIST_ATTACHMENT);
         boolean newFoodEaten = playerFoodList.addFood(usedStack);
@@ -47,11 +49,18 @@ public class FoodListEatEvent {
         if (newFoodEaten) {
 
             if (IS_DEV && ENABLE_EXTENSIVE_LOGGING) {
-                LOGGER.info("FoodListEvents.onFoodEaten: New food eaten registered: " + usedStack.getDisplayName());
+                LOGGER.info("FoodListEvents.onFoodEaten: New food eaten registered: {}", usedStack.getDisplayName());
             }
-
             //For now immediate sync. Maybe needs changing when celebration becomes a thing
             MilestoneManagerRegistry.syncFoodListAndUpdateAllManagersForPlayer(player, playerFoodList.getFoodEatenCount());
+        } else {
+            return;
+        }
+
+        List<?> celebrations = new ArrayList<>(); // TODO get from MilestoneManagerRegistry > From all Managers > from all there MilestoneProgressions.
+        boolean milestonesReached = false;
+        if (milestonesReached && EffectiveSide.get().isClient()) {
+            // TODO Celebrate
         }
     }
 }
